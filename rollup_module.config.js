a9cc812da9d1f0aa27ad601ab7350bec3516b427
Joseph Eagar <joeedh@gmail.com>
2022-01-27 15:32:59 -0800

    Minify nstructjs_es6.js, and also don't include tinyeval

--- rollup_module.config.js
+++ rollup_module.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 //import commonjs from '@rollup/plugin-commonjs';
 import cjs from "rollup-plugin-cjs-es";
 import replace from '@rollup/plugin-replace';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/structjs.js',
@@ -12,7 +13,14 @@ export default {
     name: "nstructjs"
   },
   plugins: [
+    replace({
+      '//$BUILD_TINYEVAL_START' : '/*',
+      '//$BUILD_TINYEVAL_END' : '*/',
+      changed: 'replaced',
+      delimiters: ['', '']
+    }),
     resolve(),
+    terser()
   ]
 };
 
