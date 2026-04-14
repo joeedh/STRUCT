bb314d6866f5f1023d96f9984146f4a1083fe058
Joe Eagar <joeedh@gmail.com>
2026-04-13 21:23:42 -0700

    update build_package.sh

--- build_package.sh
+++ build_package.sh
@@ -4,14 +4,11 @@ git commit -a
 
 bash build.sh
 
-mkdir -p package
+mkdir -p package/build
 mkdir -p package/docs
 
 cp -r wiki/*.md package/docs
-cp build/nstructjs.js package
-cp build/nstructjs_es6.js package
-cp build/nstructjs_tinyeval.js package
-cp build/tinyeval.js package
+cp build/* package/build
 cp package.json package
 cp README.md package
 cp LICENSE package
